4b96c447005894bd483198d44d5ea9473a6e937d
Rushil Popat <venustus.ormi@gmail.com>
2021-05-04 20:27:30 +0530

    add board and some infromation

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,15 @@
 var board1 = Chessboard('board1');
 var data = require('./960.json');
 // console.log(data[222].Chess960FEN); 
+
+var rn = require('random-number');
+var gen = rn.generator({
+  min:  0
+, max:  959
+, integer: true
+});
+
+
+var num = gen();
+var fenString = data[num].Chess960FEN;
+board1.position(fenString);
