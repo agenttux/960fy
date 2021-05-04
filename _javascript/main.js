efe154c1291508390d240bc4d32daa8d1b28ad40
Rushil Popat <venustus.ormi@gmail.com>
2021-05-04 19:46:35 +0530

    load json data

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+var board1 = Chessboard('board1');
+var data = require('./960.json');
+// console.log(data[222].Chess960FEN); 
