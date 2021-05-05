1e19193c90b16e2d9fb6c7a03d5e702542f9662a
Rushil Popat <venustus.ormi@gmail.com>
2021-05-05 09:20:10 +0530

    add random position generator

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,7 +1,8 @@
 var board1 = Chessboard('board1');
 var data = require('./960.json');
-// console.log(data[222].Chess960FEN); 
 
+
+// generate random 960 position
 var rn = require('random-number');
 var gen = rn.generator({
   min:  0
@@ -9,7 +10,17 @@ var gen = rn.generator({
 , integer: true
 });
 
+// function to set the board to a random 960 position
+function setRandomPos(){
+  var num = gen();
+  var fenString = data[num].Chess960FEN;
+  board1.position(fenString);
+}
 
-var num = gen();
-var fenString = data[num].Chess960FEN;
-board1.position(fenString);
+// initially set board to random 960 position
+setRandomPos();
+
+// on random button click, get new random position
+$('#genRandomButton').on('click', function () {
+  setRandomPos();
+})
